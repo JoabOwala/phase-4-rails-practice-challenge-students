46d4f463d1ff5af140bbf945ea892c5f8b15aecc
JoabOwala <owalajoab@gmail.com>
2023-06-29 11:05:57 +0300

    CRUD for instructor

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,12 +1,12 @@
 class InstructorsController < ApplicationController
 
-    #GET/instructos
+    #GET /instructos
     def index
         instructors = Instructor.all 
         render json: instructors
     end
 
-    #GET/instuctors/:id
+    #GET /instuctors/:id
     def show
         instructor = Instructor.find_by(id: params[:id])
         if instructor
@@ -15,4 +15,31 @@ class InstructorsController < ApplicationController
             render json: { error: 'Instructor not found' }, status: :not_found
         end
     end
+
+    #POST /instuctors
+    def create
+        instructor = Instructor.create(instructor_params)
+        render json: instructor, status: :created
+    end
+
+    #PATCH /instructors/:id
+    def update
+         instructor = Instructor.find_by(id: params[:id])
+         instructor.update(instructor_params)
+         render json: instructor
+    end
+
+    #DELETE /instructors/:id
+    def destroy
+        instructor = Instructor.find_by(id: params[:id])
+        instructor.destroy
+        head :no_content
+    end
+
+    private
+
+    def instructor_params
+        params.permit(:name)
+    end
+
 end
